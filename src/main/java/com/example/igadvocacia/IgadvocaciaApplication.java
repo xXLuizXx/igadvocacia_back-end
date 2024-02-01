fedaaf5e0369f3f829e6b0336a9f5659807bcb1a
luiz.fonseca <luiz.fonseca@basis.com.br>
2024-02-01 11:29:18 -0300

    Primeiro commit

---
+++ src/main/java/com/example/igadvocacia/IgadvocaciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.igadvocacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgadvocaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgadvocaciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/igadvocacia/IgadvocaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.igadvocacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgadvocaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
